55c9743c11855e4ab7ba5a0aefd47ee7f6d58714
SirArchibald97 <43316918+SirArchibald97@users.noreply.github.com>
2024-03-26 22:09:56 +0100

    Added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../app.css";
+    import { inject } from '@vercel/analytics';
+    import { DEV } from "$env/static/public";
+    inject({ mode: DEV ? "development" : "production" });
 </script>
 
 <main class="bg-slate-200">
