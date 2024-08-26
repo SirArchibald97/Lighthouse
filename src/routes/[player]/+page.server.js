2987cbf46e86c43bb73cfd29c411c4b8c965a2ef
Archie Margretts <archiemargretts@gmail.com>
2024-08-27 00:59:04 +0100

    Updated player redirects

--- src/routes/[player]/+page.server.js
+++ src/routes/[player]/+page.server.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export async function load({ params }) {
     const allowedPages = ['leaderboards', 'compare', 'leagues', 'discord', 'api', 'link', 'simulator'];
-    if (!allowedPages.includes(params.player)) {
+    if (!allowedPages.includes(params.player) && !params.player.includes(".")) {
         throw redirect(301, `/player/${params.player}`);
     }
 }
